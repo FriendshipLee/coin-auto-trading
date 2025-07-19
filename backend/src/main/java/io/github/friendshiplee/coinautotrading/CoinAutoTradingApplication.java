639bb054c33d35cdf7b644d0e953cae4a554667d
FriendshipLee <callhead4@naver.com>
2025-07-19 15:36:58 +0900

    application.yml, build.gradle 기본 설정

---
+++ backend/src/main/java/io/github/friendshiplee/coinautotrading/CoinAutoTradingApplication.java
@@ -0,0 +1,13 @@
+package io.github.friendshiplee.coinautotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAutoTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAutoTradingApplication.class, args);
+    }
+
+}
